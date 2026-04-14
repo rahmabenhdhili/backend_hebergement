e1a1ebe472747d7c27f04939c4a5cf8cad1b4318
Rahma Hdhili <rahma1.benhdhili@gmail.com>
2026-04-14 22:12:20 +0100

    Initial commit

---
+++ src/main/java/com/university/Hebergement/HebergementApplication.java
@@ -0,0 +1,13 @@
+package com.university.Hebergement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HebergementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HebergementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/university/Hebergement/HebergementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.Hebergement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HebergementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
